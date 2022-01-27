3466fb9b9086b21e1db47a40d6aa3d9283fdcc0a
minjuice1 <minjuice93@gmail.com>
2022-01-28 04:09:52 +0900

    modify: common/Grid 컴포넌트에 form style 추가

--- src/common/Grid.js
+++ src/common/Grid.js
@@ -20,6 +20,8 @@ const Grid = (props) => {
 		borderRight,
 		scrollY,
 		children,
+		_onSubmit,
+		checkGrid,
 	} = props;
 
 	const styles = {
@@ -40,7 +42,13 @@ const Grid = (props) => {
 		scrollY,
 	};
 
-	return <ElGrid {...styles}>{children}</ElGrid>;
+	return checkGrid === "form" ? (
+		<ElForm {...styles} onSubmit={_onSubmit}>
+			{children}
+		</ElForm>
+	) : (
+		<ElGrid {...styles}>{children}</ElGrid>
+	);
 };
 
 Grid.defaultProps = {
@@ -61,6 +69,30 @@ Grid.defaultProps = {
 };
 
 const ElGrid = styled.div`
+	width: ${(props) => props.width};
+	height: ${(props) => props.height};
+	${(props) => (props.padding ? `padding: ${props.padding};` : "")}
+	${(props) => (props.margin ? `margin: ${props.margin};` : "")}
+  ${(props) => (props.isFlex ? "display: flex;" : "")}
+  ${(props) => (props.isWrap ? "flex-wrap: wrap;" : "")}
+  ${(props) => (props.column ? "flex-direction: column;" : "")}
+	${(props) => `justify-content: ${props.justify};`};
+	${(props) => `align-items: ${props.align};`};
+	background: ${(props) => props.bg};
+	${(props) => (props.border ? `border: 2px solid ${border.primary};` : "")};
+	${(props) =>
+		props.borderBottom ? `border-bottom: 1px solid ${border.primary};` : ""};
+	${(props) =>
+		props.borderRight ? `border-right: 1px solid ${border.primary};` : ""};
+	${(props) =>
+		props.scrollY
+			? `overflow: scroll;
+      &::-webkit-scrollbar{width:8px; height:0px; background: rgba(255,255,255,0.4)};
+      &::-webkit-scrollbar-thumb{background-color:rgba(0,0,0,0.4); border-radius:6px;};`
+			: ""};
+`;
+
+const ElForm = styled.form`
 	width: ${(props) => props.width};
 	height: ${(props) => props.height};
 	${(props) => (props.padding ? `padding: ${props.padding};` : "")}
