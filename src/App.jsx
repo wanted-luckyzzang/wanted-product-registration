3fd10e55d19239c96cd7693edeb46418e6bce1a7
Ahn <81910935+BYEONGJIN-AHN@users.noreply.github.com>
2022-01-27 18:08:53 +0900

    refactor: GlobalStyle적용

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import GlobalStyle from "styles/GlobalStyle";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <div>Project Start!</div>
+    </>
+  );
+}
+
+export default App;
