c46da90b970e94d2385edf66f57126ff2d8437ae
Ahn <81910935+BYEONGJIN-AHN@users.noreply.github.com>
2022-01-28 01:31:21 +0900

    feat: 이미지 업로드(preview 없는 버전) 기능만 구현함

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -1,16 +1,31 @@
-import React, { useRef } from "react";
-import styled from "styled-components";
+import React, { useEffect, useRef, useState } from "react";
 import Button from "common/Button";
-
-const Input = styled.input`
-  display: none;
-`;
+import Input from "common/Input";
+import { v4 as uuidv4 } from "uuid";
 
 const ImageUpload = () => {
+  const [images, setImages] = useState([]);
   const imgInputRef = useRef(null);
 
   const addImageButton = () => {
     imgInputRef.current.click();
+  };
+
+  const imageDeleteButton = (e) => {
+    const deleteFileName = e.target.parentNode.textContent.replace("삭제", "");
+    setImages((prev) => {
+      const filterFiles = [...prev].filter((el) => el !== deleteFileName);
+      return filterFiles;
+    });
+  };
+
+  const imgChange = (e) => {
+    const files = e.target.files;
+    for (const idx in files) {
+      if (!isNaN(idx)) {
+        setImages((prev) => [...prev, files[idx].name]);
+      }
+    }
   };
 
   return (
@@ -21,7 +36,24 @@ const ImageUpload = () => {
         children="+ 이미지 첨부"
         color="black"
       />
-      <Input type="file" ref={imgInputRef} />
+      {images.map((el) => (
+        <div style={{ display: "flex" }} key={uuidv4()}>
+          {el}
+          <Button
+            width="50px"
+            children="삭제"
+            color="red"
+            _onClick={imageDeleteButton}
+          />
+        </div>
+      ))}
+      <Input
+        type="file"
+        _ref={imgInputRef}
+        onChange={imgChange}
+        multiple
+        none={true}
+      />
     </>
   );
 };
