e9dd565245705d45738c3340e044139be0bed736
Yena-Yun <cheryleduart@gmail.com>
2022-01-28 00:39:08 +0900

    Grid 스타일 컴포넌트에 border-radius 추가

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Grid, Button, Text } from 'common';
+import { Grid, Checkbox, Text } from 'common';
+import { bg } from 'styles/palette';
 
 export const Category = (props) => {
   return (
     <Grid width='40rem' margin='1rem auto 1rem' border isFlex column>
       <Grid height='3rem' isFlex align='center' borderBottom>
-        상품 기본 정보
+        <Text bold>상품 기본 정보</Text>
       </Grid>
       <Grid isFlex height='20rem'>
-        <Grid width='10rem' borderRight>
+        <Grid width='10rem' borderRight bg={bg.category}>
           카테고리 *
         </Grid>
-        <Grid></Grid>
+        <Grid isFlex padding='0.8rem'>
+          <Grid width='18rem' border padding='0.8rem 1.2rem' margin='0 1rem 0 0' radius>
+            <Grid isFlex align='center'>
+              <Checkbox />
+              <Text>카테고리명</Text>
+            </Grid>
+          </Grid>
+          <Grid width='14rem' border padding='0.8rem 1.2rem' radius>
+            dd
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
