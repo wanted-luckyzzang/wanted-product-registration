ab7382e9dfccf896bb6c5cfd9be8ef688f7457a5
Ahn <81910935+BYEONGJIN-AHN@users.noreply.github.com>
2022-01-28 02:32:01 +0900

    fix: 충돌난 부분 고침

--- src/common/Grid.js
+++ src/common/Grid.js
@@ -40,66 +40,48 @@ const Grid = (props) => {
     scrollY,
   };
 
-	const styles = {
-		width,
-		height,
-		padding,
-		margin,
-		isFlex,
-		isWrap,
-		column,
-		justify,
-		align,
-		bg,
-		radius,
-		border,
-		borderBottom,
-		borderRight,
-		scrollY,
-	};
-
-	return <ElGrid {...styles}>{children}</ElGrid>;
+  return <ElGrid {...styles}>{children}</ElGrid>;
 };
 
 Grid.defaultProps = {
-	width: "100%",
-	height: "auto",
-	padding: "",
-	margin: "",
-	isFlex: false,
-	isWrap: "",
-	column: false,
-	justify: "",
-	align: "",
-	bg: "#fff",
-	border: false,
-	borderBottom: false,
-	borderRight: false,
-	scrollY: "",
+  width: "100%",
+  height: "auto",
+  padding: "",
+  margin: "",
+  isFlex: false,
+  isWrap: "",
+  column: false,
+  justify: "",
+  align: "",
+  bg: "#fff",
+  border: false,
+  borderBottom: false,
+  borderRight: false,
+  scrollY: "",
 };
 
 const ElGrid = styled.div`
-	width: ${(props) => props.width};
-	height: ${(props) => props.height};
-	${(props) => (props.padding ? `padding: ${props.padding};` : "")}
-	${(props) => (props.margin ? `margin: ${props.margin};` : "")}
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  ${(props) => (props.padding ? `padding: ${props.padding};` : "")}
+  ${(props) => (props.margin ? `margin: ${props.margin};` : "")}
   ${(props) => (props.isFlex ? "display: flex;" : "")}
   ${(props) => (props.isWrap ? "flex-wrap: wrap;" : "")}
   ${(props) => (props.column ? "flex-direction: column;" : "")}
 	${(props) => `justify-content: ${props.justify};`};
-	${(props) => `align-items: ${props.align};`};
-	background: ${(props) => props.bg};
-	${(props) => (props.border ? `border: 2px solid ${border.primary};` : "")};
-	${(props) =>
-		props.borderBottom ? `border-bottom: 1px solid ${border.primary};` : ""};
-	${(props) =>
-		props.borderRight ? `border-right: 1px solid ${border.primary};` : ""};
-	${(props) =>
-		props.scrollY
-			? `overflow: scroll;
+  ${(props) => `align-items: ${props.align};`};
+  background: ${(props) => props.bg};
+  ${(props) => (props.border ? `border: 2px solid ${border.primary};` : "")};
+  ${(props) =>
+    props.borderBottom ? `border-bottom: 1px solid ${border.primary};` : ""};
+  ${(props) =>
+    props.borderRight ? `border-right: 1px solid ${border.primary};` : ""};
+  ${(props) =>
+    props.scrollY
+      ? `overflow: scroll;
       &::-webkit-scrollbar{width:8px; height:0px; background: rgba(255,255,255,0.4)};
       &::-webkit-scrollbar-thumb{background-color:rgba(0,0,0,0.4); border-radius:6px;};`
-			: ""};
+      : ""};
 `;
 
 export default Grid;
