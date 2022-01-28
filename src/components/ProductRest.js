c952198e5655df369255f6ded9ef07b546c1afe7
jiyong LEE <49055628+Jiyong95@users.noreply.github.com>
2022-01-28 20:46:00 +0900

    feat: productRest컴포넌트 추가

---
+++ src/components/ProductRest.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Grid, Text } from 'common';
+import Input from 'common/Input';
+import { bg } from 'styles/palette';
+import { v1 } from 'uuid';
+
+const ProductRest = () => {
+  return (
+    <Grid isFlex border>
+      <Grid width="20%">
+        <Grid
+          bg={bg.category}
+          height="3.5rem"
+          isFlex
+          align="center"
+          padding="0 0 0 .5rem"
+        >
+          <Text bold>상품 총 재고 *</Text>
+        </Grid>
+      </Grid>
+      <Grid
+        isFlex
+        width="80%"
+        height="3.5rem"
+        padding="0 0 0 .5rem"
+        align="center"
+      >
+        <Text>7개</Text>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default React.memo(ProductRest);
