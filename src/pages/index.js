4d51f8a4dd4a23c79cdcfcec9f18fd9555fce2ae
Ahn <81910935+BYEONGJIN-AHN@users.noreply.github.com>
2022-01-29 05:23:31 +0900

    refactor: ImageSelect 바인딩

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,13 @@
-import BasicInfo from "./BasicInfo";
-import ProductsPeriodSetting from "./ProductsPeriodSetting";
-import ProductNoticeInfo from "./ProductNoticeInfo";
-import ProductOption from "./ProductOption";
+import BasicInfo from './BasicInfo';
+import ProductsPeriodSetting from './ProductsPeriodSetting';
+import ProductNoticeInfo from './ProductNoticeInfo';
+import ProductOption from './ProductOption';
+import ImageSelect from './ImageSelect';
 
-export { BasicInfo, ProductsPeriodSetting, ProductNoticeInfo, ProductOption };
+export {
+  BasicInfo,
+  ProductsPeriodSetting,
+  ProductNoticeInfo,
+  ProductOption,
+  ImageSelect,
+};
