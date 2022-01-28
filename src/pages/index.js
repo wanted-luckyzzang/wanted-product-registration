98a6a240f53433a78a2c62b41e9d7c2dd991fc25
Ahn <81910935+BYEONGJIN-AHN@users.noreply.github.com>
2022-01-29 02:25:26 +0900

    refactor: ProductOption 바인딩

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,13 @@ import SalesPeriodSetting from './SalesPeriodSetting';
 import TogglePage from './TogglePage';
 import ProductsPeriodSetting from './ProductsPeriodSetting';
 import ProductNoticeInfo from './ProductsPeriodSetting';
+import ProductOption from './ProductOption';
 
-export { BasicInfo, SalesPeriodSetting, TogglePage, ProductsPeriodSetting, ProductNoticeInfo };
+export {
+  BasicInfo,
+  SalesPeriodSetting,
+  TogglePage,
+  ProductsPeriodSetting,
+  ProductNoticeInfo,
+  ProductOption,
+};
