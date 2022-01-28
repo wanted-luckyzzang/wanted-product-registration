f5f037d9b06686a3d88b5dbb3d3ea2a01cfcc227
Yena-Yun <cheryleduart@gmail.com>
2022-01-29 03:40:38 +0900

    add: 카테고리와 태그 번갈아가며 삭제되는 기능 구현

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,50 +1,48 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import { Grid, Text, Button } from 'common';
 import { bg, border } from 'styles/palette';
 import CATEGORY from 'utils/categoryData';
 
 const Category = () => {
   const [selectedTag, setSelectedTag] = useState([]);
+  const inputRef = useRef(null);
+
+  const onDeleteTag = useCallback(
+    (deleteTag, e) => {
+      setSelectedTag(selectedTag.filter((tag) => deleteTag !== tag));
+      inputRef.current.querySelectorAll('input').forEach((el) => {
+        if (el.value === e?.target.innerText.substring(0, e.target.innerText.length - 2)) {
+          el.checked = false;
+        }
+      });
+    },
+    [selectedTag]
+  );
 
   const checkHandler = useCallback(
     (e) => {
       if (e.target.checked && !selectedTag.includes(e.target.value)) {
         setSelectedTag([...selectedTag, e.target.value]);
+      } else if (!e.target.checked && selectedTag.includes(e.target.value)) {
+        const deleteTagId = selectedTag.indexOf(e.target.value);
+        onDeleteTag(selectedTag[deleteTagId]);
       }
     },
-    [selectedTag, setSelectedTag]
-  );
-
-  const onDeleteTag = useCallback(
-    (deleteTag) => {
-      setSelectedTag(selectedTag.filter((tag) => deleteTag !== tag));
-    },
-    [selectedTag]
+    [selectedTag, setSelectedTag, onDeleteTag]
   );
 
   return (
     <Grid isFlex column borderBottom>
-      <Grid isFlex height="20rem">
-        <Grid width="10rem" bg={bg.field} padding="1rem 0 0 1rem">
+      <Grid isFlex height='20rem'>
+        <Grid width='10rem' bg={bg.field} padding='1rem 0 0 1rem'>
           카테고리 *
         </Grid>
-        <Grid isFlex padding="0.8rem">
-          <Grid
-            width="50%"
-            border
-            padding="0.8rem 1.2rem"
-            margin="0 1rem 0 0"
-            scrollY
-          >
+        <Grid isFlex padding='0.8rem'>
+          <Grid width='50%' border padding='0.8rem 1.2rem' margin='0 1rem 0 0' _ref={inputRef} scrollY>
             {CATEGORY.map((item, idx) => {
               return (
-                <Grid key={idx} isFlex align="center" margin="0 0 0.6rem">
-                  <input
-                    id="check"
-                    type="checkbox"
-                    value={item}
-                    onChange={(e, item) => checkHandler(e, item)}
-                  />
+                <Grid key={idx} isFlex align='center' margin='0 0 0.6rem'>
+                  <input id='check' type='checkbox' value={item} onChange={checkHandler} />
                   {item}
                 </Grid>
               );
@@ -52,15 +50,11 @@ const Category = () => {
           </Grid>
 
           {selectedTag.length > 0 ? (
-            <Grid width="50%" padding="0.8rem 1.2rem" border scrollY>
+            <Grid width='50%' padding='0.8rem 1.2rem' border scrollY>
               {selectedTag.map((item, idx) => {
                 return (
-                  <Grid key={idx} isFlex align="center" margin="0 0 0.6rem">
-                    <Button
-                      _onClick={() => onDeleteTag(item)}
-                      border={border.button}
-                      bg={bg.category}
-                    >
+                  <Grid key={idx} isFlex align='center' margin='0 0 0.6rem'>
+                    <Button _onClick={(e) => onDeleteTag(item, e)} border={border.button} bg={bg.category}>
                       {item} X
                     </Button>
                   </Grid>
@@ -68,15 +62,7 @@ const Category = () => {
               })}
             </Grid>
           ) : (
-            <Grid
-              width="50%"
-              padding="0.8rem 1.2rem"
-              border
-              scrollY
-              isFlex
-              justify="center"
-              align="center"
-            >
+            <Grid width='50%' padding='0.8rem 1.2rem' border scrollY isFlex justify='center' align='center'>
               <Text>카테고리를 지정해주세요.</Text>
             </Grid>
           )}
