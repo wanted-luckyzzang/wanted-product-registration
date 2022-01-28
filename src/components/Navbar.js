a307e9646537e8865d0e156eb1326d23245546ef
Ahn <81910935+BYEONGJIN-AHN@users.noreply.github.com>
2022-01-29 05:39:05 +0900

    refactor: ImageSelect 라우팅

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,12 @@ const Navbar = () => {
 
   return (
     <>
-      <Grid width='12rem' position='sticky' borderBottom='1px solid #E3E3E3' margin='0'>
+      <Grid
+        width="12rem"
+        position="sticky"
+        borderBottom="1px solid #E3E3E3"
+        margin="0"
+      >
         <div
           style={{
             display: 'flex',
@@ -14,19 +19,36 @@ const Navbar = () => {
             height: '60%',
           }}
         >
-          <Button height='43.5px' _onClick={() => navigate(`/`)} border='none' borderBottom='1px solid #E3E3E3'>
+          <Button
+            height="43.5px"
+            _onClick={() => navigate(`/`)}
+            border="none"
+            borderBottom="1px solid #E3E3E3"
+          >
             노출 및 판매기간 설정
           </Button>
-          <Button border='none' borderBottom='1px solid #E3E3E3'>
+          <Button border="none" borderBottom="1px solid #E3E3E3">
             상품 기본 정보
           </Button>
-          <Button border='none' borderBottom='1px solid #E3E3E3' _onClick={() => navigate(`/productoption`)}>
+          <Button
+            border="none"
+            borderBottom="1px solid #E3E3E3"
+            _onClick={() => navigate(`/productoption`)}
+          >
             상품 옵션
           </Button>
-          <Button border='none' borderBottom='1px solid #E3E3E3'>
+          <Button
+            border="none"
+            borderBottom="1px solid #E3E3E3"
+            _onClick={() => navigate(`/imageselect`)}
+          >
             상품 등록
           </Button>
-          <Button border='none' borderBottom='1px solid #E3E3E3' _onClick={() => navigate(`/noticeInfo`)}>
+          <Button
+            border="none"
+            borderBottom="1px solid #E3E3E3"
+            _onClick={() => navigate(`/noticeInfo`)}
+          >
             상품 정보 고시
           </Button>
         </div>
