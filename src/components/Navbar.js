1c6bf5646e1bbc4f322ed9e6ad333bdce0dada73
Ahn <81910935+BYEONGJIN-AHN@users.noreply.github.com>
2022-01-29 04:34:46 +0900

    feat: /productoption 라우팅 설정

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,12 @@ const Navbar = () => {
   const navigate = useNavigate();
   return (
     <>
-      <Grid width='12rem' position='sticky' borderBottom='1px solid #E3E3E3' margin='0'>
+      <Grid
+        width="12rem"
+        position="sticky"
+        borderBottom="1px solid #E3E3E3"
+        margin="0"
+      >
         <div
           style={{
             display: 'flex',
@@ -12,19 +17,32 @@ const Navbar = () => {
             height: '60%',
           }}
         >
-          <Button height='43.5px' _onClick={() => navigate(`/`)} border='none' borderBottom='1px solid #E3E3E3'>
+          <Button
+            height="43.5px"
+            _onClick={() => navigate(`/`)}
+            border="none"
+            borderBottom="1px solid #E3E3E3"
+          >
             노출 및 판매기간 설정
           </Button>
-          <Button border='none' borderBottom='1px solid #E3E3E3'>
+          <Button border="none" borderBottom="1px solid #E3E3E3">
             상품 기본 정보
           </Button>
-          <Button border='none' borderBottom='1px solid #E3E3E3'>
+          <Button
+            border="none"
+            borderBottom="1px solid #E3E3E3"
+            _onClick={() => navigate(`/productoption`)}
+          >
             상품 옵션
           </Button>
-          <Button border='none' borderBottom='1px solid #E3E3E3'>
+          <Button border="none" borderBottom="1px solid #E3E3E3">
             상품 등록
           </Button>
-          <Button border='none' borderBottom='1px solid #E3E3E3' _onClick={() => navigate(`/noticeInfo`)}>
+          <Button
+            border="none"
+            borderBottom="1px solid #E3E3E3"
+            _onClick={() => navigate(`/noticeInfo`)}
+          >
             상품 정보 고시
           </Button>
         </div>
