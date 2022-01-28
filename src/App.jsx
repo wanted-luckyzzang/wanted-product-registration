b5621ff42d386b45a09cded666774c1c52933482
Yena-Yun <cheryleduart@gmail.com>
2022-01-28 22:21:24 +0900

    merge 에러 추가 수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { BasicInfo, TogglePage, ProductsPeriodSetting, ProductNoticeInfo } from 'pages';
 import 'styles/App.css';
-import { Layout, Filtertag } from 'components';
+import { Layout, FilterTag, ImageUpload } from 'components';
 
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path='/' element={<TogglePage />} />
-				{/* <Route path='/' element={<BasicInfo />} /> */}
-				<Route path="/filtertag" element={<FilterTag />} />
-				<Route path="/noticeInfo" element={<ProductNoticeInfo />} />
-				<Route path="/imageupload" element={<ImageUpload />} />
+        {/* <Route path='/' element={<BasicInfo />} /> */}
+        <Route path='/filtertag' element={<FilterTag />} />
+        <Route path='/noticeInfo' element={<ProductNoticeInfo />} />
+        <Route path='/imageupload' element={<ImageUpload />} />
       </Routes>
     </Layout>
   );
+}
 
-	export default App;
+export default App;
--- src/common/Button.js
+++ src/common/Button.js
@@ -3,37 +3,21 @@ import icon from 'assets/deleteIcon.png';
 import { bg, text, border } from 'styles/palette';
 
 export default function Button(props) {
-	const {
-		width,
-		height,
-		margin,
-		bg,
-		color,
-		border,
-		children,
-		_onClick,
-		fontWeight,
-		bold,
-		type,
-		sx,
-		borderBottom,
-		borderTop,
-	} = props;
+  const { width, height, margin, bg, color, border, children, _onClick, fontWeight, bold, type, sx, borderBottom, borderTop } = props;
 
-	const styles = {
-		width,
-		height,
-		margin,
-		bg,
-		color,
-		border,
-		fontWeight,
-		bold,
-		borderBottom,
-		borderTop,
-	};
+  const styles = {
+    width,
+    height,
+    margin,
+    bg,
+    color,
+    border,
+    fontWeight,
+    bold,
+    borderBottom,
+    borderTop,
+  };
 
-<<<<<<< HEAD
   return (
     <>
       {type === 'button' ? (
@@ -53,56 +37,27 @@ export default function Button(props) {
             cursor: 'pointer',
             ...sx,
           }}
-          onClick={onClick}
+          onClick={_onClick}
         >
           <img src={icon} style={{ width: '1.1rem' }} alt='product' />
         </div>
       )}
     </>
   );
-=======
-	return (
-		<>
-			{type === "button" ? (
-				<ElButton {...styles} type={type} onClick={_onClick}>
-					{children}
-				</ElButton>
-			) : (
-				<div
-					style={{
-						width: "1.3rem",
-						height: "1.3rem",
-						borderRadius: "50%",
-						border: "1px solid #e3e3e3",
-						display: "flex",
-						justifyContent: "center",
-						alignItems: "center",
-						cursor: "pointer",
-						...sx,
-					}}
-					onClick={_onClick}
-				>
-					<img src={icon} style={{ width: "1.1rem" }} />
-				</div>
-			)}
-		</>
-	);
->>>>>>> main
 }
 
 Button.defaultProps = {
-	width: "100%",
-	height: "40px",
-	children: null,
-	bg: "#fff",
-	color: "#000",
-	type: "button",
-	border: "1px solid #e3e3e3",
-	_onClick: () => {},
+  width: '100%',
+  height: '40px',
+  children: null,
+  bg: '#fff',
+  color: '#000',
+  type: 'button',
+  border: '1px solid #e3e3e3',
+  _onClick: () => {},
 };
 
 const ElButton = styled.button`
-<<<<<<< HEAD
   display: flex;
   align-items: center;
   justify-content: center;
@@ -113,20 +68,4 @@ const ElButton = styled.button`
   ${(props) => props.bg && `background: ${props.bg};`};
   ${(props) => props.border && `border: 1px solid ${props.border};`}
   border-radius: 4px;
-=======
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	width: ${(props) => props.width};
-	height: ${(props) => props.height};
-	${(props) => (props.margin ? `margin: ${props.margin};` : "")}
-	${(props) => (props.color ? `color: ${props.color};` : "")}
-  ${(props) => (props.bg ? `background: ${props.bg};` : "")};
-	${(props) => (props.border ? `border: ${props.border};` : "")}
-	border-radius: 4px;
-	font-weight: ${(props) => (props.bold ? "700" : "400")};
-	margin: ${(props) => props.margin};
-	${(props) => (props.border ? `border-bottom: ${props.borderBottom};` : "")}
-	${(props) => (props.border ? `border-top: ${props.borderTop};` : "")}
->>>>>>> main
 `;
