5fd2cffcd6d9ea3ba9857cbfca2ae5a6156e3ba5
minjuice1 <minjuice93@gmail.com>
2022-01-29 03:29:34 +0900

    feat: pages/noticeInfo 정보고시 추가버튼누르면 정보고시 값 받아오기

--- src/common/Button.js
+++ src/common/Button.js
@@ -1,88 +1,85 @@
-import styled from 'styled-components';
-import icon from 'assets/deleteIcon.png';
+import styled from "styled-components";
+import icon from "assets/deleteIcon.png";
 
 export default function Button(props) {
-  const {
-    width,
-    height,
-    margin,
-    bg,
-    color,
-    border,
-    children,
-    _onClick,
-    fontWeight,
-    bold,
-    type,
-    sx,
-    borderBottom,
-    borderTop,
-    justify,
-  } = props;
+	const {
+		width,
+		height,
+		margin,
+		bg,
+		color,
+		border,
+		children,
+		_onClick,
+		fontWeight,
+		bold,
+		type,
+		sx,
+		borderBottom,
+		borderTop,
+	} = props;
 
-  const styles = {
-    width,
-    height,
-    margin,
-    bg,
-    color,
-    border,
-    fontWeight,
-    bold,
-    borderBottom,
-    borderTop,
-    justify,
-  };
+	const styles = {
+		width,
+		height,
+		margin,
+		bg,
+		color,
+		border,
+		fontWeight,
+		bold,
+		borderBottom,
+		borderTop,
+	};
 
-  return (
-    <>
-      {type === 'delete' && (
-        <div
-          style={{
-            width: '1.3rem',
-            height: '1.3rem',
-            borderRadius: '50%',
-            border: '1px solid #e3e3e3',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            cursor: 'pointer',
-            ...sx,
-          }}
-          onClick={_onClick}
-        >
-          <img src={icon} style={{ width: '1.1rem' }} alt="product" />
-        </div>
-      )}
-      <ElButton {...styles} type={type} onClick={_onClick}>
-        {children}
-      </ElButton>
-    </>
-  );
+	return (
+		<>
+			{type === "delete" && (
+				<div
+					style={{
+						width: "1.3rem",
+						height: "1.3rem",
+						borderRadius: "50%",
+						border: "1px solid #e3e3e3",
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+						cursor: "pointer",
+						...sx,
+					}}
+					onClick={_onClick}
+				>
+					<img src={icon} style={{ width: "1.1rem" }} alt="product" />
+				</div>
+			)}
+			<ElButton {...styles} type={type} onClick={_onClick}>
+				{children}
+			</ElButton>
+		</>
+	);
 }
 
 Button.defaultProps = {
-  width: '100%',
-  height: '40px',
-  children: null,
-  bg: '#fff',
-  color: '#000',
-  type: '',
-  border: '1px solid #e3e3e3',
-  justify: 'center',
-  _onClick: () => {},
+	width: "100%",
+	height: "40px",
+	children: null,
+	bg: "#fff",
+	color: "#000",
+	type: "button",
+	border: "1px solid #e3e3e3",
+	_onClick: () => {},
 };
 
 const ElButton = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: ${(props) => props.justify}
-  width: ${(props) => props.width};
-  height: ${(props) => props.height};
-  ${(props) => props.margin && `margin: ${props.margin};`};
-  ${(props) => props.color && `color: ${props.color};`};
-  ${(props) => props.bg && `background: ${props.bg};`};
-  ${(props) => props.border && `border: 1px solid ${props.border};`};
-  border-radius: 4px;
-  font-weight: ${(props) => (props.bold ? '700' : '400')};
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: ${(props) => props.width};
+	height: ${(props) => props.height};
+	${(props) => props.margin && `margin: ${props.margin};`};
+	${(props) => props.color && `color: ${props.color};`};
+	${(props) => props.bg && `background: ${props.bg};`};
+	${(props) => props.border && `border: 1px solid ${props.border};`};
+	border-radius: 4px;
+	font-weight: ${(props) => (props.bold ? "700" : "400")};
 `;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import BasicInfo from "./BasicInfo";
 import ProductsPeriodSetting from "./ProductsPeriodSetting";
-import ProductNoticeInfo from "./ProductsPeriodSetting";
+import ProductNoticeInfo from "./ProductNoticeInfo";
 import ProductOption from "./ProductOption";
 
 export { BasicInfo, ProductsPeriodSetting, ProductNoticeInfo, ProductOption };
