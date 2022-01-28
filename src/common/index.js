1dc517963727fbc9cda4a005d4a64e718afa267d
minjuice1 <minjuice93@gmail.com>
2022-01-28 15:57:52 +0900

    refactor: common/Grid form에서 안쓰는 속성 수정

--- src/common/index.js
+++ src/common/index.js
@@ -1,9 +1,10 @@
-import Grid from './Grid';
-import Button from './Button';
-import Input from './Input';
-import Text from './Text';
-import Radio from './Radio';
-import Checkbox from './Checkbox';
-import Tag from './Tag';
+import Grid from "./Grid";
+import Button from "./Button";
+import Input from "./Input";
+import Text from "./Text";
+import Radio from "./Radio";
+import Checkbox from "./Checkbox";
+import Tag from "./Tag";
+import Container from "./Container";
 
-export { Grid, Button, Input, Text, Radio, Checkbox, Tag };
+export { Grid, Button, Input, Text, Radio, Checkbox, Tag, Container };
